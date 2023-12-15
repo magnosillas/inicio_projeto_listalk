894466037fc556225515e151d5c552b051058106
Giudicelli Elias <86128363+giudicellisilva@users.noreply.github.com>
2023-12-14 21:03:34 -0300

    Adicionando style no componente Home

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,7 +3,7 @@ import styles from "./home.module.scss";
 const Home = ({setVisible}) =>{
 
     return(
-        <section>
+        <section className={styles.home}>
             <h1>Home</h1>
             <button onClick={() => setVisible(true)} >Login</button>
         </section>
